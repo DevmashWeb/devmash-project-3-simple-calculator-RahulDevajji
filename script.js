ca104c452d9fb16e4e4d072c55a7666d4dcc9a07
RahulDevajji <devajjir@gmail.com>
2016-09-29 21:18:31 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
 function show1(){
 
-	document.case.display.value += "1"
+	document.calc.display.value += "1"
 };
 function show2(){
 
-	document.case.display.value += "2"
+	document.calc.display.value += "2"
 };
 function show3(){
 
-	document.case.display.value += "3"
+	document.calc.display.value += "3"
 };
 function show4(){
 
-	document.case.display.value += "4"
+	document.calc.display.value += "4"
 };
 function show5(){
 
-	document.case.display.value += "5"
+	document.calc.display.value += "5"
 };
 
 function show6(){
 
-	document.case.display.value += "6"
+	document.calc.display.value += "6"
 };
 
 function show7(){
 
-	document.case.display.value += "7"
+	document.calc.display.value += "7"
 };
 
 function show8(){
 
-	document.case.display.value += "8"
+	document.calc.display.value += "8"
 };
 
 function show9(){
 
-	document.case.display.value += "9"
+	document.calc.display.value += "9"
 };
 
 function show0(){
 
-	document.case.display.value += "0"
+	document.calc.display.value += "0"
 };
 function add(){
 
-	document.case.display.value += "+"
+	document.calc.display.value += "+"
 };
 function sub(){
 
-	document.case.display.value += "-"
+	document.calc.display.value += "-"
 };
 function div(){
 
-	document.case.display.value += "/"
+	document.calc.display.value += "/"
 };
 function mul(){
 
-	document.case.display.value += "*"
+	document.calc.display.value += "*"
 };
 
 function clearD(){
 
-	document.case.display.value = ""
+	document.calc.display.value = ""
 };
 function solve(){
-	var equal = eval(document.case.display.value)
-	document.case.display.value = equal;
+	var equal = eval(document.calc.display.value)
+	document.calc.display.value = equal;
 }
